6cd44862b909f2f048055f8b5500b09c9f71bac9
marmos91 <marco.moschettini@cubbit.io>
2025-05-13 10:43:03 +0200

    fix local editor css import

---
+++ app/declarations.d.ts
@@ -0,0 +1,9 @@
+// declarations.d.ts
+declare module '*.module.css' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
+declare module '*.module.scss' {
+    const classes: { [key: string]: string };
+    export default classes;
+}
