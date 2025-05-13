db61c6a2bade7437fbca7db82648fbf594edcbeb
marmos91 <marco.moschettini@cubbit.io>
2025-05-13 11:06:26 +0200

    remove Elemento logo

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -9,14 +9,13 @@ const orbitron = Orbitron({
     style: 'normal',
 });
 
-const h1Copy = 'Upload a photo to Cubbit DS3 Powered by Elemento';
+const h1Copy = 'Upload a photo to Cubbit DS3';
 
 const UploadPage = () => {
     return (
         <main className={[styles.uploadPage, orbitron.className].join(' ')}>
             <div className={styles.logo}>
                 <Image src="/cubbit.png" alt="Cubbit logo" width={60} height={80} priority />
-                <Image src="/elemento.png" alt="Elemento logo" width={80} height={80} priority />
             </div>
             <h1 className={styles.h1}>{h1Copy}</h1>
             <UploadForm />
