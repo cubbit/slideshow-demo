2d973b6396a0fbed673093efe1e6830e598915ba
marmos91 <marco.moschettini@cubbit.io>
2025-05-15 15:54:11 +0200

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
+            </head>
             <body suppressHydrationWarning={true}>{children}</body>
         </html>
     );
