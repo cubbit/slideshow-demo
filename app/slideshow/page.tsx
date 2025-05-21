a884525378c22991e2ca7f7c176485f0357c2510
marmos91 <marco.moschettini@cubbit.io>
2025-05-21 09:37:44 +0200

    Change h1

--- app/slideshow/page.tsx
+++ app/slideshow/page.tsx
@@ -4,7 +4,6 @@ import styles from './page.module.css';
 import { Orbitron } from 'next/font/google';
 import InfiniteCarousel from '../components/carousel/InfiniteCarousel';
 import Image from 'next/image';
-import { usePublicSettings } from '@/app/hooks/usePublicSettings';
 
 const orbitron = Orbitron({
     weight: '500',
@@ -12,14 +11,10 @@ const orbitron = Orbitron({
     style: 'normal',
 });
 
+const h1 = `This is your geo-distributed data`;
+
 export default function SlideshowPage() {
     const today = new Date();
-
-    // Use settings hook to get the latest S3 settings
-    const { settings } = usePublicSettings();
-
-    const s3Endpoint = settings.S3_ENDPOINT || '';
-    const s3Bucket = settings.S3_BUCKET_NAME || '';
 
     const formattedDate = today.toLocaleDateString(undefined, {
         weekday: 'long',
@@ -33,10 +28,8 @@ export default function SlideshowPage() {
             <header className={styles.header}>
                 <Image src="/cubbit.png" alt="Cubbit logo" width={30} height={40} priority />
 
-                <h1>{formattedDate}</h1>
-                {s3Endpoint !== '' && s3Bucket !== '' && (
-                    <span>{`URL: ${s3Endpoint}/${s3Bucket}`}</span>
-                )}
+                <h1>{h1}</h1>
+                <span>{formattedDate}</span>
             </header>
 
             <div className={styles['carousel-container']}>
